a2b60f9de66817946b0f3ee416ce477e17d86ee5
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2017-01-13 11:38:40 +0100

    Initialize project

---
+++ src/Algorithm/NearestNeighborhoodAlgorithm.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using NearestNeighborhood.Data;
+
+namespace NearestNeighborhood.Algorithm
+{
+    public class NearestNeighborhoodAlgorithm
+    {
+        private readonly int _k;
+        private readonly UserToSongMap _userToSongMap;
+        private Dictionary<string, SortedSet<SimilarUser>> _similarityMap;
+
+        public NearestNeighborhoodAlgorithm(UserToSongMap userToSongMap, int k = 100)
+        {
+            _userToSongMap = userToSongMap;
+            _k = k;
+            _similarityMap = new Dictionary<string, SortedSet<SimilarUser>>();
+        }
+
+        public Dictionary<string, SortedSet<SimilarUser>> Find(int forUsers = -1)
+        {
+            string[] users;
+
+            if (forUsers > 0)
+                users = _userToSongMap.Users.Take(_k).ToArray();
+            else
+                users = _userToSongMap.Users.ToArray();
+
+            foreach (var user in users)
+            {
+                var userSongs = _userToSongMap.GetSongsForUser(user);
+
+                foreach (var similarUser in _userToSongMap.Users)
+                {
+                    var similarUserSongs = _userToSongMap.GetSongsForUser(similarUser);
+
+                    var similarity = MeasuereSimiliarity(userSongs, similarUserSongs);
+
+                    AddSimilarUser(user, similarity, similarUser);
+                }
+            }
+
+            return _similarityMap;
+        }
+
+        private static void AddSimilarUser(double similarity, string similarUserId, SortedSet<SimilarUser> similarUsers)
+        {
+            similarUsers.Add(new SimilarUser(similarUserId, similarity));
+        }
+
+        private void AddSimilarUser(string userId, double similarity, string similarUserId)
+        {
+            if (_similarityMap.ContainsKey(userId))
+            {
+                InserSimilarUser(userId, similarity, similarUserId);
+            }
+            else
+            {
+                _similarityMap[userId] = new SortedSet<SimilarUser>(SimilarUser.SimilarUserComparer);
+            }
+        }
+
+        private void InserSimilarUser(string userId, double similarity, string similarUserId)
+        {
+            var similarUsers = _similarityMap[userId];
+            if (similarUsers.Count == _k)
+            {
+                ReplaceWithLoweSimilarity(similarity, similarUserId, similarUsers);
+            }
+            else
+            {
+                AddSimilarUser(similarity, similarUserId, similarUsers);
+            }
+        }
+
+        private double MeasuereSimiliarity(ICollection<string> firstUserSongs, ICollection<string> secondUserSongs)
+        {
+            var intersectedCount = (double)firstUserSongs.Intersect(secondUserSongs).Count();
+            var uninonCount = (double)firstUserSongs.Union(secondUserSongs).Count();
+
+            return intersectedCount / uninonCount;
+        }
+
+        private void ReplaceWithLoweSimilarity(double similarity, string similarUserId, SortedSet<SimilarUser> similarUsers)
+        {
+            if (!(similarity > similarUsers.Min.Similarity))
+                return;
+            similarUsers.Remove(similarUsers.Min);
+            AddSimilarUser(similarity, similarUserId, similarUsers);
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using NearestNeighborhood.Algorithm;
+using NearestNeighborhood.Data;
+using Newtonsoft.Json;
+
+namespace NearestNeighborhood
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            var dataPath = ConfigurationSettings.AppSettings["dataPath"];
+            var resultPath = ConfigurationSettings.AppSettings["resultPath"];
+
+            var dataLoader = new DataLoader(dataPath, 1000);
+
+            stopwatch.Start();
+
+            var userToSongMap = dataLoader.Data;
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Loaded {nameof(userToSongMap)}  in {stopwatch.ElapsedMilliseconds} ms.");
+
+            Console.ReadKey();
+
+            var knearest = new NearestNeighborhoodAlgorithm(userToSongMap);
+
+            var result = knearest.Find(100);
+
+            WriteResults(result, resultPath);
+        }
+
+        private static void WriteResults(Dictionary<string, SortedSet<SimilarUser>> result, string resultPath)
+        {
+            var jsonResult = JsonConvert.SerializeObject(result);
+
+            using (var stream = File.Create(resultPath))
+            {
+                using (var streamWriter = new StreamWriter(stream))
+                {
+                    streamWriter.Write(jsonResult);
+                }
+            }
+        }
+    }
+}
